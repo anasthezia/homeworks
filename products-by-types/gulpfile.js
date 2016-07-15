2a77f5343924351684c37f5ed51e0f91f914a2ab
anasthezia <anasthezia161@gmail.com>
2016-07-15 18:03:08 +0300

    start hw1

--- products-by-types/gulpfile.js
+++ products-by-types/gulpfile.js
@@ -8,6 +8,11 @@ var gulp = require('gulp'), // Сообственно Gulp JS
     uglify = require('gulp-uglify'), // Минификация JS
     concat = require('gulp-concat') // Склейка файлов
      sourcemaps = require('gulp-sourcemaps');
+     watch = require('gulp-watch'),
+     livereload = require('gulp-livereload'), // Livereload для Gulp
+     lr = require('tiny-lr'), // Минивебсервер для livereload
+     connect = require('connect'), // Webserver
+    server = lr(),
     browserSync = require('browser-sync').create();
 
     gulp.task('browser-sync', function() {
@@ -26,31 +31,91 @@ var gulp = require('gulp'), // Сообственно Gulp JS
     });
     gulp.task('styles', function() {
         return gulp.src('src/less/*.less')
+            .pipe(less())
+            .pipe(concat('all.css'))
+            .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+            .pipe(gulp.dest('./dist/css/'))
+            .pipe(browserSync.stream())
+            .pipe(livereload(server));
+    });
+        gulp.task('styles-prod', function() {
+        return gulp.src('src/less/*.less')
             .pipe(sourcemaps.init())
             .pipe(less())
             .pipe(concat('all.css'))
             .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-           // .pipe(csso())
+            .pipe(csso())
+            .pipe(sourcemaps.write())
             .pipe(gulp.dest('./dist/css/'))
-            .pipe(browserSync.stream());
     });
 
-    gulp.task('images', function() {
+    gulp.task('copy-images', function() {
         return gulp.src('src/img/*')
-            .pipe(imagemin())
             .pipe(gulp.dest('./dist/img'));
     });
 
+    gulp.task('min-images', function() {
+        return gulp.src('src/img/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('./dist/img'));
+
+    });
 
     // Собираем JS
     gulp.task('scripts', function() {
-        gulp.src(['src/js/*.js'])
+        return gulp.src(['src/js/*.js'])
+            .pipe(concat('all.js')) // Собираем все JS, кроме тех которые находятся в ./assets/js/vendor/**
+            .pipe(sourcemaps.init())
+            .pipe(gulp.dest('./dist/js'))
+            .pipe(browserSync.stream())
+             .pipe(livereload(server));
+    });
+    gulp.task('scripts-prod', function() {
+        return gulp.src(['src/js/*.js'])
             .pipe(sourcemaps.init())
             .pipe(concat('all.js')) // Собираем все JS, кроме тех которые находятся в ./assets/js/vendor/**
-            .pipe(gulp.dest('./dist/js'))
             .pipe(uglify())
-            .pipe(browserSync.stream());
-    });
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('./dist/js'))
 
+
+    });
+    gulp.task('copy-html', function(){
+        return gulp.src('src/*.html')
+            .pipe(gulp.dest('./dist/'))
+            .pipe(livereload(server));
+    });
+    // Локальный сервер для разработки
+    gulp.task('http-server', function() {
+        connect()
+            .use(require('connect-livereload')())
+            .use(connect.static('./dist'))
+            .listen('9000');
+
+        console.log('Server listening on http://localhost:9000');
+    }); 
+
+
+    gulp.task('watch', function() {
+
+        watch(['./src/less/*.less'], function () {
+            gulp.start('styles');
+        });
+
+         watch(['./src/*.html'], function () {
+            gulp.start('copy-html');
+         });
+
+         watch(['./src/js/*.js'], function () {
+            gulp.start('scripts');
+        });
+
+        watch(['./src/img/*.*'], function () {
+            gulp.start('copy-images');
+        });
+    });
     // *** Default task
-    gulp.task('default', ['styles', 'scripts', 'images']);
+    gulp.task('default', ['copy-html', 'styles', 'scripts', 'copy-images'] );
+    gulp.task('prod', ['copy-html', 'styles-prod', 'scripts-prod', 'min-images']);
+
+
