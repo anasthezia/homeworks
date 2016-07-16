06b409df3d431334a475aa9b67999fea31b4573f
anasthezia <anasthezia161@gmail.com>
2016-07-16 20:57:45 +0300

    add product sorting script

--- products-by-types/src/js/script.js
+++ products-by-types/src/js/script.js
@@ -1,4 +1,43 @@
 'use strict';
 $(function () {
     // Ваш код
+     $.ajax({
+        dataType: 'json',
+        url: 'products.json',
+        beforeSend: function () {
+            $.fancybox.showLoading();
+        }
+
+        }).done(function (data){
+        var types = {
+	            sale : "Распродажа",
+	            promo : "Промо-акция",
+	            recommended : "Рекомендуемые товары"
+       		 };
+        var sale = [];
+		var promo = [];
+		var recommended = [];
+		data.forEach(function(item)  {
+			if (item.type=="sale"){
+				sale.push(item); 
+			} else if (item.type=="promo"){
+				promo.push(item);
+			}else if (item.type=="recommended"){
+				recommended.push(item);
+			}
+		});
+
+
+            var tmpl = _.template(document.getElementById( 'productTemplate' ).innerHTML);
+
+            $( '#sale' ).append( tmpl({ title: "Распродажа", products: sale }) );
+            $( '#promo' ).append( tmpl({ title: "Промо-акция", products: promo }) );
+            $( '#recommended' ).append( tmpl({ title: "Рекомендованные товары", products: recommended }) );
+			})
+        .fail(function() {
+             console.log("Произошла какая-то ошибка");
+        })
+        .always(function(){
+            $.fancybox.hideLoading();
+        });
 });
